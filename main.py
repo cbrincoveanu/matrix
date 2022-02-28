ef4a4bc73480bde8415221d73fb3701a8f2a137f
cbrincoveanu <c.brincoveanu@yahoo.com>
2022-02-28 17:58:02 +0100

    add numbers

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ ROW_SIZE = 15
 COLS_NUM = int(WIDTH / COL_SIZE)
 KATAKANA_FONT_SIZE = 23
 KATAKANA_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijkl!<"
+SECOND_FONT_SIZE = 23
+SECOND_CHARS = "0123456789!<>=+-"
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-katakana_font = pygame.font.Font("katakana.ttf", KATAKANA_FONT_SIZE)  # pygame.font.SysFont(None, FONT_SIZE)
+katakana_font = pygame.font.Font("katakana.ttf", KATAKANA_FONT_SIZE)
+second_font = pygame.font.SysFont(None, SECOND_FONT_SIZE)
 clock = pygame.time.Clock()
 
 columns = [0 for _ in range(COLS_NUM)]
@@ -27,9 +30,10 @@ while True:
         if random.random() < 0.01:
             c = (255, 255, 255)
         letter = katakana_font.render(random.choice(list(KATAKANA_CHARS)), True, c)
+        if random.random() < 0.1:
+            letter = second_font.render(random.choice(list(SECOND_CHARS)), True, c)
         letter = pygame.transform.flip(letter, True, False)
-        screen.blit(letter,
-                    (i * COL_SIZE, column * ROW_SIZE))
+        screen.blit(letter, (i * COL_SIZE, column * ROW_SIZE))
         columns[i] += 1
         if columns[i] > 30 + random.random() * 2000:
             columns[i] = 0
